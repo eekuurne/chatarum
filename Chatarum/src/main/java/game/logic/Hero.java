d583793f96da98d1a0bd0d55eef81bf1cac6f3d2
EERO KUURNE <eero.kuurne@helsinki.fi>
2015-01-30 11:29:57 +0200

    Luokkakaavio jne

---
+++ Chatarum/src/main/java/game/logic/Hero.java
@@ -0,0 +1,6 @@
+
+package game.logic;
+
+public class Hero {
+    
+}
---
+++ Chatarum/src/main/java/game/logic/Player.java
@@ -0,0 +1,18 @@
+
+package game.logic;
+
+public class Player {
+    
+    private Deck deck;
+    private Hand hand;
+    private Table table;
+    private Hero hero;
+    private int health;
+    private int faction;
+
+    
+    
+    
+    
+    
+}
